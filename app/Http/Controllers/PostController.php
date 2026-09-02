957b0d41acf175a62e5c250f9e5e024427be0723
Alif Taran Ihsan <aliftaranihsan@gmail.com>
2026-09-02 22:31:05 +0700

    Belajar Laravel12 - Hello Laravel

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Post;
+
+class PostController extends Controller {
+    public function index() {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+}
