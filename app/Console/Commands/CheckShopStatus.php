d4ae874af432918dbfb5231df38bafcfcad094d7
Alif Taran Ihsan <aliftaranihsan@gmail.com>
2026-09-06 06:41:47 +0700

    Add initial implementation of console commands, controllers, middleware, models, factories, and migrations for product management

---
+++ app/Console/Commands/CheckShopStatus.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CheckShopStatus extends Command
+{
+    protected $signature = 'pos:status {jam?}';
+    protected $description = 'Mengecek status operasional Toko Kelontong POS';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $kasir = $this->ask('Masukkan Nama Anda: ');
+        $jam = $this->argument('jam') ?? 10;
+        $this->info("=== SISTEM MONITORING TOKO KELONTONG ===");
+
+        if($jam >= 8 && $jam <= 21) {
+            $this->info("Halo $kasir, Status Toko pada jam $jam:00 WIB adalah: BUKA");
+            $this->comment("Silakan kasir bersiap di meja transaksi.");
+            } else {
+                $this->error("Halo $kasir, Status Toko pada jam $jam:00 WIB adalah: TUTUP");
+                $this->warn("Akses transaksi kasir dinonaktifkan sementara.");
+            }
+}
+}
